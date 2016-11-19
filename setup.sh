de210fe2e8cfefb3114c9fb43ae273c547cb4a82
pensun <Peng.Sun@amd.com>
2016-11-19 00:20:35 -0600

    Add notes regarding hotkeys

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 sudo apt-get install zsh
 chsh -s /bin/zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cd
-touch .zshrc
 echo "ZSH_THEME="3den"" > ~/zshrc
 sudo apt-get install tmux
+sudo gem install tmuxinator
+export EDITOR='vim'
+
 cp ./tmux.conf ~/.tmux.conf
 cp ./zshrc ~/.zshrc
